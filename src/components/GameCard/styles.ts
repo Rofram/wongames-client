23be5c7f94aef9ee2090b03d60a8c8fda357e8ad
Rodrigo de França <howdrigo.fr@gmail.com>
2021-10-14 20:46:07 -0300

    refactor: 🎉 fix image height and add border radius

--- src/components/GameCard/styles.ts
+++ src/components/GameCard/styles.ts
@@ -8,37 +8,46 @@ export const Wrapper = styled.article`
     width: 100%;
     height: 100%;
     background-color: ${theme.colors.white};
+    border-radius: ${theme.border.radius};
   `}
 `
 
 export const ImageBox = styled.div`
-  height: 14rem;
-  width: 100%;
-  background: #f6f7f8;
-  background-image: linear-gradient(
-    to right,
-    #f6f7f8 0%,
-    #edeef1 20%,
-    #f6f7f8 40%,
-    #f6f7f8 100%
-  );
-  background-size: 80rem 14rem;
-  animation: placeholderShimmer 1s linear infinite forwards;
-
-  img {
+  ${({ theme }) => css`
+    min-height: 14rem;
+    position: relative;
     width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
+    border-radius: ${theme.border.radius} ${theme.border.radius} 0 0;
+    background: #f6f7f8;
+    background-image: linear-gradient(
+      to right,
+      #f6f7f8 0%,
+      #edeef1 20%,
+      #f6f7f8 40%,
+      #f6f7f8 100%
+    );
+    background-size: 80rem 14rem;
+    animation: placeholderShimmer 1s linear infinite forwards;
 
-  @keyframes placeholderShimmer {
-    0% {
-      background-position: -40rem 0;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
     }
-    100% {
-      background-position: 40rem 0;
+
+    > div {
+      border-radius: ${theme.border.radius} ${theme.border.radius} 0 0;
     }
-  }
+
+    @keyframes placeholderShimmer {
+      0% {
+        background-position: -40rem 0;
+      }
+      100% {
+        background-position: 40rem 0;
+      }
+    }
+  `}
 `
 
 export const Content = styled.div`
