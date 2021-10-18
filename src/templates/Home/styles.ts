625f0933b0e6025934721e4b9c76e61264a4974d
Rodrigo de França <howdrigo.fr@gmail.com>
2021-10-18 02:07:28 -0300

    fix heading color in mobile

--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -41,6 +41,11 @@ export const SectionBanner = styled.section`
 export const SectionNews = styled(Sections)`
   ${({ theme }) => css`
     margin-bottom: calc(${theme.spacings.xxlarge} * 2);
+
+    ${HeadingStyles.Wrapper} {
+      color: ${theme.colors.white};
+    }
+
     ${media.greaterThan('large')`
       margin-top: -13rem;
     `}
