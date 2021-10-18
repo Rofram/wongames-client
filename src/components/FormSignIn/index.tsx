f4826f0bf75bb27daf4116b151c29a3b76368b9c
Rodrigo de França <howdrigo.fr@gmail.com>
2021-10-18 01:52:18 -0300

    feat: 🎉 implemented FormSignIn component

---
+++ src/components/FormSignIn/index.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link'
+import { Email, Lock } from '@styled-icons/material-outlined'
+
+import { FormWrapper, FormLink } from 'components/Form'
+import Button from 'components/Button'
+import TextField from 'components/TextField'
+
+import * as S from './styles'
+
+const FormSignIn = () => (
+  <FormWrapper>
+    <form>
+      <TextField
+        name="email"
+        type="email"
+        placeholder="Email"
+        icon={<Email />}
+      />
+      <TextField
+        name="password"
+        type="password"
+        placeholder="Password"
+        icon={<Lock />}
+      />
+
+      <S.ForgotPassword href="#">Forgot your password?</S.ForgotPassword>
+
+      <Button size="large" fullWidth>
+        Sign in now
+      </Button>
+
+      <FormLink>
+        Don&apos;t have an account?{' '}
+        <Link href="/sign-up">
+          <a>Sign up</a>
+        </Link>
+      </FormLink>
+    </form>
+  </FormWrapper>
+)
+
+export default FormSignIn
---
+++ src/components/FormSignIn/test.tsx
@@ -0,0 +1,36 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import FormSignIn from '.'
+
+describe('<FormSignIn />', () => {
+  it('should render the form', () => {
+    const { container } = renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
+
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('button', { name: /sign in now/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the forgot password link', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(
+      screen.getByRole('link', { name: /Forgot your password/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the text to sign up', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByText(/have an account/i)).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /sign up/i })).toBeInTheDocument()
+  })
+})
