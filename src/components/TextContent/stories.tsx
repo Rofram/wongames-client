85d1405d061ab2553606285b5af58604f296aa1a
Rodrigo de França <howdrigo.fr@gmail.com>
2021-10-21 20:20:03 -0300

    feat: 🎉 implemented TextContent component

---
+++ src/components/TextContent/stories.tsx
@@ -0,0 +1,19 @@
+import { Story, Meta } from '@storybook/react'
+import TextContent, { TextContentProps } from '.'
+
+import mock from './mock'
+
+export default {
+  title: 'TextContent',
+  component: TextContent,
+  args: mock,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<TextContentProps> = (args) => (
+  <TextContent {...args} />
+)
