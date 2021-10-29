da4c40875cf2687c96777c880786e847f0a544aa
Rodrigo de França <howdrigo.fr@gmail.com>
2021-10-29 16:49:00 -0300

    add empty component

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -7,6 +7,7 @@ import { Divider } from 'components/Divider'
 import GameCard, { GameCardProps } from 'components/GameCard'
 import { HighlightProps } from 'components/Highlight'
 import Showcase from 'components/Showcase'
+import Empty from 'components/Empty'
 
 export type WishlistTemplateProps = {
   games?: GameCardProps[]
@@ -15,7 +16,7 @@ export type WishlistTemplateProps = {
 }
 
 const Wishlist = ({
-  games,
+  games = [],
   recommendedGames,
   recommendedHighlightedGames
 }: WishlistTemplateProps) => (
@@ -25,11 +26,19 @@ const Wishlist = ({
         Wishlist
       </Heading>
 
-      <Grid>
-        {games?.map((game, index) => (
-          <GameCard key={`wishlist-${index}`} {...game} />
-        ))}
-      </Grid>
+      {games.length > 0 ? (
+        <Grid>
+          {games.map((game, index) => (
+            <GameCard key={`wishlist-${index}`} {...game} />
+          ))}
+        </Grid>
+      ) : (
+        <Empty
+          title="Your wishlist is empty"
+          description="Games added to your wishlist will appear here"
+          hasLink
+        />
+      )}
 
       <Divider />
     </Container>
--- src/templates/Wishlist/test.tsx
+++ src/templates/Wishlist/test.tsx
@@ -24,4 +24,14 @@ describe('<Wishlist />', () => {
 
     expect(screen.getByTestId('Showcase')).toBeInTheDocument()
   })
+
+  it('should render empty when there are no games', () => {
+    renderWithTheme(<Wishlist {...props} games={undefined} />)
+
+    expect(screen.queryByText(/population zero/i)).not.toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /your wishlist is empty/i })
+    ).toBeInTheDocument()
+  })
 })
