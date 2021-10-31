e9c0c3a3dd5fe533cec7c998e14e919a8620c54a
Rodrigo de França <howdrigo.fr@gmail.com>
2021-10-30 21:59:48 -0300

    updatecartlist mock

--- src/components/CartList/mock.ts
+++ src/components/CartList/mock.ts
@@ -3,10 +3,15 @@ import { CartListProps } from '.'
 import gameItemMock, { paymentInfoMock } from 'components/GameItem/mock'
 
 const mock: CartListProps = {
+  items: Array.from({ length: 5 }, () => gameItemMock),
+  total: '$250,00'
+}
+
+export const withPaymentInfo: CartListProps = {
   items: Array.from({ length: 5 }, () =>
     Object.assign(gameItemMock, { paymentInfo: paymentInfoMock })
   ),
-  total: '$250,00'
+  total: '$300,00'
 }
 
 export default mock
