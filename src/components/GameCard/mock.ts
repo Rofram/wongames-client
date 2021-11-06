ba4efc3afdca9e943f190d3228726f0fcc1f4523
Rodrigo de França <howdrigo.fr@gmail.com>
2021-11-06 17:33:47 -0300

    add gameCardmock

---
+++ src/components/GameCard/mock.ts
@@ -0,0 +1,11 @@
+import { GameCardProps } from '.'
+
+const mock: GameCardProps = {
+  title: 'Super Mario Odyssey',
+  developer: 'Nintendo',
+  img: 'https://images.unsplash.com/photo-1612404459571-ccef4b6588e7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=140',
+  price: 'R$ 235,00',
+  promotionalPrice: 'R$ 200,00'
+}
+
+export default mock
