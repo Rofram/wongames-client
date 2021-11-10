e36592448b27cd0894ed5d6e87094a22248cc695
Rodrigo de França <howdrigo.fr@gmail.com>
2021-11-10 18:27:09 -0300

    feat: create apollo hock

---
+++ src/utils/apollo.ts
@@ -0,0 +1,40 @@
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  NormalizedCacheObject
+} from '@apollo/client'
+import { useMemo } from 'react'
+
+let apolloClient: ApolloClient<NormalizedCacheObject>
+
+function createApolloClient() {
+  return new ApolloClient({
+    ssrMode: typeof window === 'undefined',
+    link: new HttpLink({ uri: 'http://localhost:1337/graphql' }),
+    cache: new InMemoryCache()
+  })
+}
+
+export function initializeApollo(initialState = {}) {
+  // If the current state is an object then use it
+  const apolloClientGlobal = apolloClient ?? createApolloClient()
+
+  // restore cache from localStorage
+  if (initialState) {
+    apolloClientGlobal.cache.restore(initialState)
+  }
+
+  // For SSR initialize the cache with clean data
+  if (typeof window === 'undefined') return apolloClientGlobal
+
+  apolloClient = apolloClient ?? apolloClientGlobal
+
+  return apolloClient
+}
+
+export function useApollo(initialState = {}) {
+  const store = useMemo(() => initializeApollo(initialState), [initialState])
+
+  return store
+}
