c6209c17b8f9e6d4483c654da0a68004ab80d340
Rodrigo de França <howdrigo.fr@gmail.com>
2021-12-26 18:16:44 -0300

    feat: 🎉 implemented banner, game, home fragments

---
+++ src/graphql/queries/home.ts
@@ -0,0 +1,34 @@
+import { gql } from '@apollo/client'
+import { BannerFragment } from 'graphql/fragments/banner'
+import { GameFragment } from 'graphql/fragments/game'
+
+export const QUERY_HOME = gql`
+  query QueryHome {
+    banners {
+      ...BannerFragment
+    }
+
+    newGames: games(
+      where: { release_date_lte: "2021-11-14" }
+      sort: "release_date:desc"
+      limit: 8
+    ) {
+      ...GameFragment
+    }
+
+    upcomingGames: games(
+      where: { release_date_gt: "2021-11-14" }
+      sort: "release_date:asc"
+      limit: 8
+    ) {
+      ...GameFragment
+    }
+
+    freeGames: games(where: { price: 0 }, sort: "release_date:desc", limit: 8) {
+      ...GameFragment
+    }
+  }
+
+  ${BannerFragment}
+  ${GameFragment}
+`
