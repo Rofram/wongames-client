9edcb52f0bf1d6e8c58b88f58ce604d37872226f
Rodrigo de França <howdrigo.fr@gmail.com>
2021-12-26 18:18:24 -0300

    feat: 🎉 implemented partial data api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,58 @@
+import { QueryHome } from 'graphql/generated/QueryHome'
+import { QUERY_HOME } from 'graphql/queries/home'
 import HomeTemplate, { HomeTemplateProps } from 'templates/Home'
 
-import homePropsMock from 'templates/Home/mock'
+import { initializeApollo } from 'utils/apollo'
+
+import gameCardSliderMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
 
 export default function Index(props: HomeTemplateProps) {
   return <HomeTemplate {...props} />
 }
 
-export function getStaticProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryHome>({
+    query: QUERY_HOME
+  })
+
   return {
-    props: homePropsMock
+    props: {
+      revalidate: 60,
+      banners: data.banners.map((banner) => ({
+        img: banner.image?.url,
+        title: banner.title,
+        subtitle: banner.subtitle,
+        buttonLabel: banner.button?.label,
+        buttonLink: banner.button?.link,
+        ...(banner.ribbon && {
+          ribbon: banner.ribbon.text,
+          ribbonColor: banner.ribbon.color,
+          ribbonSize: banner.ribbon.size
+        })
+      })),
+      newsGames: data.newGames.map((game) => ({
+        slug: game.slug,
+        title: game.name,
+        developer: game.developers[0].name || '',
+        img: game.cover?.url,
+        price: game.price
+      })),
+      mostPopularHighlight: highlightMock,
+      mostPopularGames: gameCardSliderMock,
+      upcomingGames: data.upcomingGames.map((game) => ({
+        slug: game.slug,
+        title: game.name,
+        developer: game.developers[0].name || 'desconhecido',
+        img: game.cover?.url,
+        price: game.price
+      })),
+      upcomingHighlight: highlightMock,
+      upcomingMoreGames: gameCardSliderMock,
+      FreeGames: gameCardSliderMock,
+      FreeGamesHighlight: highlightMock
+    }
   }
 }
