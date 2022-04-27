f43857df74764bf7029b291aa9fdc31552ad1856
Rodrigo de França <howdrigo.fr@gmail.com>
2022-04-27 19:16:51 -0300

    feat: add page title and implement apollo query

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -2,13 +2,41 @@ import CartTemplate from 'templates/Cart'
 import type { CartTemplateProps } from 'templates/Cart'
 
 import cartTemplateMock from 'templates/Cart/mock'
+import { initializeApollo } from 'utils/apollo'
+import { QueryRecommended } from 'graphql/generated/QueryRecommended'
+import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
+import { gameMapper } from 'utils/mappers'
+import { GetServerSideProps, GetServerSidePropsResult } from 'next'
+import Head from 'next/head'
 
 export default function CartPage(props: CartTemplateProps) {
-  return <CartTemplate {...props} />
+  return (
+    <>
+      <Head>
+        <title>Wongames | Cart</title>
+      </Head>
+      <CartTemplate {...props} />
+    </>
+  )
 }
 
-export async function getServerSideProps() {
+export const getServerSideProps: GetServerSideProps = async (): Promise<
+  GetServerSidePropsResult<CartTemplateProps>
+> => {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryRecommended>({
+    query: QUERY_RECOMMENDED
+  })
+
   return {
-    props: cartTemplateMock
+    props: {
+      ...cartTemplateMock,
+      recommendedGames: {
+        title: data.recommended?.section?.title,
+        games: gameMapper(data.recommended?.section?.games),
+        highlighted: data.recommended?.section?.highlight
+      }
+    } as CartTemplateProps
   }
 }
