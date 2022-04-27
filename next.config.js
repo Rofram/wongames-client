eb1ecc2983d7c67a1f654e515e4c3745003991af
Rodrigo de França <howdrigo.fr@gmail.com>
2022-04-27 04:21:30 -0300

    chore: add @uiball/loaders, next-compose-plugins and next-tranpile-modules

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
+const withTM = require('next-transpile-modules')(['@uiball/loaders'])
+const withPlugins = require('next-compose-plugins')
 
-module.exports = withPWA({
+module.exports = withPlugins([withPWA,withTM], {
   pwa: {
     dest: 'public',
     disable: !isProd
