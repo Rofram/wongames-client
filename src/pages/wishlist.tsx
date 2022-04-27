1d32b4635211a3ed7d428883aa3637bce5a2fd1b
Rodrigo de França <howdrigo.fr@gmail.com>
2022-04-27 19:57:26 -0300

    feat: add page title and apollo

--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -1,14 +1,43 @@
+import { initializeApollo } from 'utils/apollo'
+import { QueryRecommended } from 'graphql/generated/QueryRecommended'
+import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
+import { GetStaticProps, GetStaticPropsResult } from 'next'
+import { gameMapper } from 'utils/mappers'
+
 import WishlistTemplate from 'templates/Wishlist'
 import type { WishlistTemplateProps } from 'templates/Wishlist'
 
 import wishlistMock from 'templates/Wishlist/mock'
+import Head from 'next/head'
 
 export default function Index(props: WishlistTemplateProps) {
-  return <WishlistTemplate {...props} />
+  return (
+    <>
+      <Head>
+        <title>Wongames | Wishlist</title>
+      </Head>
+      <WishlistTemplate {...props} />
+    </>
+  )
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async (): Promise<
+  GetStaticPropsResult<WishlistTemplateProps>
+> => {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryRecommended>({
+    query: QUERY_RECOMMENDED
+  })
+
   return {
-    props: wishlistMock
+    props: {
+      games: wishlistMock.games,
+      recommendedGames: {
+        title: data.recommended?.section?.title,
+        games: gameMapper(data.recommended?.section?.games),
+        highlighted: data.recommended?.section?.highlight
+      }
+    } as WishlistTemplateProps
   }
 }
