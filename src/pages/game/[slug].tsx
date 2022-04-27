a6d2aefac2af551c077c40a5d468bebb084a77b6
Rodrigo de França <howdrigo.fr@gmail.com>
2022-04-27 20:47:09 -0300

    feat: add page title and recommended games data

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -1,18 +1,29 @@
 import { useRouter } from 'next/router'
-import type { GetStaticProps } from 'next'
+import type { GetStaticProps, GetStaticPropsResult } from 'next'
 import { initializeApollo } from 'utils/apollo'
-import Game from 'templates/Game'
+import GameTemplate from 'templates/Game'
 import type { GameTemplateProps } from 'templates/Game'
 
-import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
+import type {
+  QueryGames,
+  QueryGamesVariables
+} from 'graphql/generated/QueryGames'
 import { QUERY_GAMES, QUERY_GAME_BY_SLUG } from 'graphql/queries/games'
-import {
+import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
+import type {
   QueryGameBySlug,
   QueryGameBySlugVariables
 } from 'graphql/generated/QueryGameBySlug'
 
-import gamesMock from 'components/GameCardSlider/mock'
+import { gameMapper } from 'utils/mappers'
 import highlightMock from 'components/Highlight/mock'
+import type { QueryRecommended } from 'graphql/generated/QueryRecommended'
+import type {
+  QueryUpcoming,
+  QueryUpcomingVariables
+} from 'graphql/generated/QueryUpcoming'
+import { QUERY_UPCOMING } from 'graphql/queries/upcoming'
+import Head from 'next/head'
 
 const apolloClient = initializeApollo()
 
@@ -21,14 +32,21 @@ export default function Index(props: GameTemplateProps) {
 
   if (router.isFallback) return null // TODO: loading
 
-  return <Game {...props} />
+  return (
+    <>
+      <Head>
+        <title>{props.gameInfo.title} on Wongames</title>
+      </Head>
+      <GameTemplate {...props} />
+    </>
+  )
 }
 
 export async function getStaticPaths() {
   const { data } = await apolloClient.query<QueryGames, QueryGamesVariables>({
     query: QUERY_GAMES,
     variables: {
-      limit: 9
+      limit: 15
     }
   })
 
@@ -41,7 +59,9 @@ export async function getStaticPaths() {
   return { paths, fallback: true }
 }
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
+export const getStaticProps: GetStaticProps = async ({
+  params
+}): Promise<GetStaticPropsResult<GameTemplateProps>> => {
   const { data } = await apolloClient.query<
     QueryGameBySlug,
     QueryGameBySlugVariables
@@ -58,17 +78,33 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
   const game = data.games[0]
 
+  const { data: recommendedData } = await apolloClient.query<QueryRecommended>({
+    query: QUERY_RECOMMENDED
+  })
+
+  const today = new Date(2020, 10, 17).toISOString().slice(0, 10)
+
+  const { data: upcomingData } = await apolloClient.query<
+    QueryUpcoming,
+    QueryUpcomingVariables
+  >({
+    query: QUERY_UPCOMING,
+    variables: {
+      date: today
+    }
+  })
+
   return {
     props: {
       revalidate: 60,
-      cover: `http://localhost:1337${game.cover?.src}`,
+      cover: game.cover?.src,
       gameInfo: {
         title: game.name,
         description: game.short_description,
         price: game.price
       },
       gallery: game.gallery.map(({ src, label }) => ({
-        src: `http://localhost:1337${src}`,
+        src: src,
         alt: label
       })),
       description: game.description,
@@ -78,11 +114,19 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
         platforms: game.platforms.map(({ name }) => name),
         publisher: game.publisher?.name,
         rating: game.rating,
-        genres: game.categories.map(({ name }) => name)
+        genres: game.categories.map(({ name }) => name!)
       },
-      upcomingGames: gamesMock,
+      upcomingGames: {
+        title: 'Upcoming Games',
+        games: gameMapper(upcomingData.games),
+        highlight: upcomingData.home?.upcomingGames?.highlight
+      },
       upcomingHighlight: highlightMock,
-      recommendedGames: gamesMock
-    }
+      recommendedGames: {
+        title: recommendedData.recommended?.section?.title,
+        highlight: recommendedData.recommended?.section?.highlight,
+        games: gameMapper(recommendedData.recommended?.section?.games)
+      }
+    } as GameTemplateProps
   }
 }
