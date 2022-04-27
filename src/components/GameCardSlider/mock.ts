8e03dd0af1582562d4530c431ca18f7a81a9a19b
Rodrigo de França <howdrigo.fr@gmail.com>
2022-04-27 16:36:35 -0300

    feat: fix duplicate key slug

--- src/components/GameCardSlider/mock.ts
+++ src/components/GameCardSlider/mock.ts
@@ -1,7 +1,7 @@
 import { GameCardProps } from 'components/GameCard'
 
-const mock: GameCardProps[] = Array.from({ length: 10 }, () => ({
-  slug: 'population-zero',
+const mock: GameCardProps[] = Array.from({ length: 10 }, (_, index) => ({
+  slug: `population-zero-${index}`,
   title: 'Population Zero',
   developer: 'Rockstar Games',
   img: 'https://source.unsplash.com/user/willianjusten/300x145',
