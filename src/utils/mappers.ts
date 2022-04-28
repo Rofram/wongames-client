811625f617926153fd10fdd9b45d2856b7bbb1d1
Rodrigo de França <howdrigo.fr@gmail.com>
2022-04-27 21:11:13 -0300

    fix: fix gameMapper undefined developer

--- src/utils/mappers.ts
+++ src/utils/mappers.ts
@@ -1,7 +1,9 @@
-import { GameFragment } from 'graphql/generated/GameFragment'
+import type { BannerProps } from 'components/Banner'
+import type { GameCardProps } from 'components/GameCard'
+import type { GameFragment } from 'graphql/generated/GameFragment'
 import type { QueryHome_banners } from 'graphql/generated/QueryHome'
 
-export const bannerMapper = (banners: QueryHome_banners[]) => {
+export const bannerMapper = (banners: QueryHome_banners[]): BannerProps[] => {
   return banners.map((banner) => ({
     img: banner.image?.url,
     title: banner.title,
@@ -13,19 +15,21 @@ export const bannerMapper = (banners: QueryHome_banners[]) => {
       ribbonColor: banner.ribbon.color,
       ribbonSize: banner.ribbon.size
     })
-  }))
+  })) as BannerProps[]
 }
 
-export const gameMapper = (games: GameFragment[] | null | undefined) => {
+export const gameMapper = (
+  games: GameFragment[] | null | undefined
+): GameCardProps[] | null => {
   if (!games) {
-    return []
+    return null
   }
 
   return games.map((game) => ({
     slug: game.slug,
     title: game.name,
-    developer: game.developers[0].name,
+    developer: game.developers[0]?.name ?? 'desconhecido',
     img: game.cover?.url,
     price: game.price
-  }))
+  })) as GameCardProps[]
 }
